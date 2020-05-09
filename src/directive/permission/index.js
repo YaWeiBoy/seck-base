91fffb78639f0eff6528c12c64db9aeb3d21ef25
zyw <1289704497@qq.com>
2020-05-09 10:35:48 +0800

    删除console

--- src/directive/permission/index.js
+++ src/directive/permission/index.js
@@ -1,6 +1,5 @@
 import permission from './permission'
 const install = function(Vue) {
-  console.log(Vue)
   Vue.directive('permission', permission)
 }
 
@@ -8,7 +7,6 @@ if (window.Vue) {
   window['permission'] = permission
   Vue.use(install); // eslint-disable-line
 }
-console.log(permission)
 permission.install = install
-console.log(permission)
 export default permission
+
