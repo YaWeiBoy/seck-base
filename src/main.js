643ad391211384a75a1e9f7a9b758c6fec896457
zyw <1289704497@qq.com>
2020-05-09 10:37:20 +0800

    element-ui尺寸

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-
+import Cookies from 'js-cookie'
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 
 import ElementUI from 'element-ui'
@@ -31,7 +31,9 @@ if (process.env.NODE_ENV === 'production') {
 // set ElementUI lang to EN
 // Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
-Vue.use(ElementUI)
+Vue.use(ElementUI, {
+  size: Cookies.get('size') || 'small'
+})
 
 Vue.config.productionTip = false
 
